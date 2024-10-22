7548120d414f9fe21ef0bda497599124d6dd59ad
Dotan Simha <dotansimha@gmail.com>
2024-10-22 11:47:51 +0300

    wip

--- packages/libraries/router/src/app_deployments.rs
+++ packages/libraries/router/src/app_deployments.rs
@@ -5,17 +5,32 @@ use schemars::JsonSchema;
 use serde::Deserialize;
 use tower::BoxError;
 
-pub struct AppDeploymentsPlugin {}
+pub struct AppDeploymentsPlugin {
+    configuration: Config,
+}
 
 #[derive(Clone, Debug, Deserialize, JsonSchema)]
-pub struct Config {}
+pub struct Config {
+    /// Default: true
+    enabled: Option<bool>,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            enabled: Some(true),
+        }
+    }
+}
 
 #[async_trait::async_trait]
 impl Plugin for AppDeploymentsPlugin {
     type Config = Config;
 
     async fn new(init: PluginInit<Config>) -> Result<Self, BoxError> {
-        todo!("this")
+        Ok(AppDeploymentsPlugin {
+            configuration: init.config,
+        })
     }
 }
 
